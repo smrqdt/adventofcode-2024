511471e97a7384cf5ecb4be5b8346f6cfe6916e3
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-12-03 20:52:14 +0100

    day03, part2

--- day03/day03.go
+++ day03/day03.go
@@ -12,6 +12,7 @@ var input string
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -34,4 +35,37 @@ func part1() {
 	}
 
 	fmt.Printf("(Part 1) Sum of all multiplications: %d\n", resultSum)
+}
+
+func part2() {
+	pattern := regexp.MustCompile(`(do(?:n't)?\(\))|(?:mul\((\d+),(\d+)\))`)
+	matches := pattern.FindAllStringSubmatch(input, -1)
+
+	var resultSum int
+
+	mulEnabled := true
+	for _, match := range matches {
+		if match[0] == "do()" {
+			mulEnabled = true
+			continue
+		}
+		if match[0] == "don't()" {
+			mulEnabled = false
+			continue
+		}
+
+		var nums []int
+		for _, value := range match[2:] {
+			num, err := strconv.Atoi(value)
+			if err != nil {
+				panic(err)
+			}
+			nums = append(nums, num)
+		}
+		if mulEnabled {
+			resultSum += nums[0] * nums[1]
+		}
+	}
+
+	fmt.Printf("(Part 2) Sum of all multiplications: %d\n", resultSum)
 }
