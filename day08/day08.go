f878d9d9f4fe7fffbec54cf6d8c64af81336704b
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-12-08 11:53:35 +0100

    day08, part2

--- day08/day08.go
+++ day08/day08.go
@@ -15,6 +15,7 @@ var input string
 func main() {
 	antennas, mapSize := parse()
 	part1(antennas, mapSize)
+	part2(antennas, mapSize)
 }
 
 func parse() (map[rune][]v.Vector, v.Vector) {
@@ -41,8 +42,8 @@ func parse() (map[rune][]v.Vector, v.Vector) {
 
 func part1(antennas map[rune][]v.Vector, mapSize v.Vector) {
 	locationsWithAntinodes := make(map[v.Vector]bool)
-	for freq, antennas := range antennas {
-		antinodes := findAntinodes(freq, antennas, mapSize)
+	for _, antennas := range antennas {
+		antinodes := findAntinodes(antennas, mapSize, calcFirstAntinodes)
 		for _, antinode := range antinodes {
 			locationsWithAntinodes[antinode] = true
 		}
@@ -51,29 +52,58 @@ func part1(antennas map[rune][]v.Vector, mapSize v.Vector) {
 	fmt.Printf("(Part 1) %d unique locations with antinodes\n", len(locationsWithAntinodes))
 }
 
-func findAntinodes(freq rune, antennas []v.Vector, mapSize v.Vector) []v.Vector {
+func part2(antennas map[rune][]v.Vector, mapSize v.Vector) {
+	locatonsWithAntinodes := make(map[v.Vector]bool)
+	for _, antennas := range antennas {
+		antinodes := findAntinodes(antennas, mapSize, calcAllAntinodes)
+		for _, antinode := range antinodes {
+			locatonsWithAntinodes[antinode] = true
+		}
+	}
+
+	fmt.Printf("(Part 2) %d unique locations with antinodes\n", len(locatonsWithAntinodes))
+}
+
+func findAntinodes(antennas []v.Vector, mapSize v.Vector, antinodeFunc func(v.Vector, v.Vector, v.Vector) []v.Vector) []v.Vector {
 	var antinodes []v.Vector
 	for _, antenna1 := range antennas {
 		for _, antenna2 := range antennas {
 			if antenna1 == antenna2 {
 				continue
 			}
-			antinodeCandidates := calcAntinodes(antenna1, antenna2)
-			for _, aC := range antinodeCandidates {
-				if isInMap(aC, mapSize) {
-					antinodes = append(antinodes, aC)
-				}
-			}
+			antinodes = append(antinodes, antinodeFunc(antenna1, antenna2, mapSize)...)
 		}
 	}
 	return antinodes
 }
 
-func calcAntinodes(antenna1, antenna2 v.Vector) []v.Vector {
+func calcFirstAntinodes(antenna1, antenna2, mapSize v.Vector) (antinodes []v.Vector) {
 	diff := antenna1.Sub(antenna2)
-	antinode1 := antenna1.Add(diff)
-	antinode2 := antenna2.Sub(diff)
-	return []v.Vector{antinode1, antinode2}
+	antinode := antenna1.Add(diff)
+	if isInMap(antinode, mapSize) {
+		antinodes = append(antinodes, antinode)
+	}
+	antinode = antenna2.Sub(diff)
+	if isInMap(antinode, mapSize) {
+		antinodes = append(antinodes, antinode)
+	}
+	return antinodes
+}
+
+func calcAllAntinodes(antenna1, antenna2, mapSize v.Vector) (antinodes []v.Vector) {
+	diff := antenna1.Sub(antenna2)
+	antinodes = append(antinodes, antenna1, antenna2)
+	antinode := antenna1.Add(diff)
+	for isInMap(antinode, mapSize) {
+		antinodes = append(antinodes, antinode)
+		antinode = antinode.Add(diff)
+	}
+	antinode = antenna2.Sub(diff)
+	for isInMap(antinode, mapSize) {
+		antinodes = append(antinodes, antinode)
+		antinode = antinode.Sub(diff)
+	}
+	return antinodes
 }
 
 func isInMap(vec v.Vector, mapSize v.Vector) bool {
