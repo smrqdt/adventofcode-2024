4fe88d5b28fcdb3a004d9920a832f60d9d73a2e2
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-12-11 17:28:29 +0100

    day11, part2

--- day11/day11.go
+++ day11/day11.go
@@ -3,7 +3,9 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"iter"
 	"log"
+	"maps"
 	"math"
 	"slices"
 	"strconv"
@@ -15,7 +17,8 @@ var input string
 
 func main() {
 	stones := parse()
-	part1(stones)
+	part1(slices.Clone(stones))
+	part2(slices.Clone(stones))
 }
 
 func parse() []int {
@@ -32,7 +35,8 @@ func parse() []int {
 }
 
 func part1(stones []int) {
-	for r := range 25 {
+	rounds := 25
+	for r := range rounds {
 		fmt.Printf("Round %3d: ", r)
 		var inserts int
 		for i := range len(stones) {
@@ -52,5 +56,46 @@ func part1(stones []int) {
 		}
 		fmt.Printf("%6d\n", len(stones))
 	}
-	fmt.Printf("(Part 1) %d \n", len(stones))
+	fmt.Printf("(Part 1) Number of stones after %d rounds: %d\n", rounds, len(stones))
+}
+
+func part2(stones []int) {
+	rounds := 75
+
+	stoneCountsA := make(map[int]int)
+	stoneCountsB := make(map[int]int)
+	for _, stone := range stones {
+		stoneCountsA[stone]++
+	}
+
+	source := &stoneCountsA
+	target := &stoneCountsB
+	fmt.Println(source)
+	for round := range rounds {
+		fmt.Printf("Round %2d: ", round)
+		clear(*target)
+		for stone, count := range *source {
+			digits := int(math.Log10(float64(stone))) + 1
+			switch {
+			case stone == 0:
+				(*target)[1] += count
+			case digits%2 == 0 && stone != 1:
+				splitFactor := int(math.Pow10(digits / 2))
+				(*target)[stone/splitFactor] += count
+				(*target)[stone%splitFactor] += count
+			default:
+				(*target)[stone*2024] += count
+			}
+		}
+		fmt.Printf("%18d \n", sum(maps.Values(*target)))
+		source, target = target, source
+	}
+	fmt.Printf("(Part 2) Number of stones after %d rounds: %d\n", rounds, sum(maps.Values(*source)))
+}
+
+func sum(seek iter.Seq[int]) (sum int) {
+	for value := range seek {
+		sum += value
+	}
+	return
 }
